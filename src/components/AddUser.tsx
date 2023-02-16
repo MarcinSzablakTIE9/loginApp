cfa09aee48c866f7435e01bb2fa2e767f038be8e
aaaa <aaaa@aaa.aaa>
2023-02-16 15:19:13 +0100

    aaaa

--- src/components/AddUser.tsx
+++ src/components/AddUser.tsx
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from '../UI/Card';
+import classes from './AddUser.module.css' ;
+import Button from '../UI/Button'
 
 const AddUser = () =>{
 
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+
+    const namedChangeHandler = (event) =>{
+        setName(event.target.value)
+    }
+
+    const ageChangeHandler = (event) =>{
+        setAge(event.target.value)
+    }
+
+    const addUserHandler = (event) =>{
+        event.preventDefault()
+        console.log(name,age)
+        setName('')
+        setAge('')
+    }
+
     return(
-        <form>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text"/>
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input 
+                    id="username" 
+                    type="text"
+                    onChange = {namedChangeHandler}
+                    value = {name}
+                />
 
-            <label htmlFor="age">Age</label>
-            <input id="age" type="number"/>
+                <label htmlFor="age">Age</label>
+                <input 
+                    id="age" 
+                    type="Number" 
+                    onChange={ageChangeHandler}
+                    value = {age}
+                />
 
-            <button type="submit">Add User</button>
-        </form>
+                <Button myType="submit"> Add user </Button>
+            </form>
+        </Card>
     )
 }
 
